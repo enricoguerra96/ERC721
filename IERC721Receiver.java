c984b995c9bca8c102e25d521ec612d4ee746ece
Enrico <43131160+enricoguerra96@users.noreply.github.com>
2021-05-24 21:23:26 +0200

    Delete IERC721Receiver.java

--- IERC721Receiver.java
+++
@@ -1,8 +0,0 @@
-package io.takamaka.ERC721;
-
-import io.takamaka.code.lang.Contract;
-import io.takamaka.code.math.UnsignedBigInteger;
-
-public interface IERC721Receiver {
-    public byte onERC721Received(Contract operator, Contract from, UnsignedBigInteger tokenId, byte Data);
-}
